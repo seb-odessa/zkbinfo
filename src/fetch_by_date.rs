f4e1504acc6574a7eea4f35bbc86f306f720348a
Pavel Kolievatykh <pavlo.kolievatykh@dxc.com>
2022-06-15 11:09:31 +0300

    fetch by date implemented

--- src/fetch_by_date.rs
+++ src/fetch_by_date.rs
@@ -1,16 +1,12 @@
-// use actix_web::middleware::Logger;
-// use actix_web::{web, App, HttpServer};
-// use anyhow::anyhow;
 use chrono::NaiveDate;
 use env_logger;
-use log::info;
+use log::{error, info, warn};
+use tokio::time::Duration;
 
 use std::collections::HashMap;
 use std::env;
 
-// use lib::api;
-// use lib::database;
-// use lib::killmail;
+use lib::killmail;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
@@ -23,29 +19,59 @@ async fn main() -> anyhow::Result<()> {
 
     let args = env::args().collect::<Vec<String>>();
     if args.len() != 1 {
-        let zkbinfo_api = format!("http://{host}:{port}/killmail/save");
-        info!("zkbinfo API url: {zkbinfo_api}");
+        let client = reqwest::Client::new();
+        let zkbinfo_save_api = format!("http://{host}:{port}/killmail/save");
+        info!("zkbinfo API SAVE url: {zkbinfo_save_api}");
 
         if let Ok(date) = NaiveDate::parse_from_str(args[1].as_str(), "%Y-%m-%d") {
+            let zkbinfo_get_saved_api = format!(
+                "http://{host}:{port}/api/killmail/saved/{}/",
+                date.format("%Y-%m-%d").to_string()
+            );
+            info!("zkbinfo API GET_SAVED: {zkbinfo_get_saved_api}");
+
             let zkb_api = format!(
                 "https://zkillboard.com/api/history/{}.json",
                 date.format("%Y%m%d").to_string()
             );
-
             info!("zkillboard.com API: {zkb_api}");
 
-            let map = reqwest::get(&zkb_api)
+            let mut map = reqwest::get(&zkb_api)
                 .await?
                 .json::<HashMap<i32, String>>()
                 .await?;
             info!("Received {} killmails from zkillboard.com", map.len());
 
-            let saved: Vec<u32> = reqwest::get(&zkbinfo_api)
+            let saved = reqwest::get(&zkbinfo_get_saved_api)
                 .await?
-                .json()
+                .json::<Vec<killmail::Key>>()
                 .await?;
             info!("Received {} killmails from zkbinfo", saved.len());
 
+            for id in saved {
+                map.remove(&id);
+            }
+            info!("The rest of killmails to receive {}", map.len());
+
+            for (id, hash) in map {
+                let evetech_api = format!(
+                    "https://esi.evetech.net/latest/killmails/{id}/{hash}/?datasource=tranquility"
+                );
+                info!("EVETECH API: {evetech_api}");
+                let killmail = reqwest::get(&evetech_api)
+                    .await?
+                    .json::<killmail::Killmail>()
+                    .await?;
+                let mut timeout = 10;
+                while let Err(what) = client.post(&zkbinfo_save_api).json(&killmail).send().await {
+                    error!("{what}");
+                    warn!("Will wait for {timeout} seconds");
+                    tokio::time::sleep(Duration::from_secs(timeout)).await;
+                    if timeout < 300 {
+                        timeout += 30;
+                    }
+                }
+            }
             return Ok(());
         }
     }
