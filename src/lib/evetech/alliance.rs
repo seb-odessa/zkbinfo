1c43ffd4caa14241dba355637625577b17d07256
Pavel Kolievatykh <pavlo.kolievatykh@dxc.com>
2022-06-18 13:09:03 +0300

    Work on character info

---
+++ src/lib/evetech/alliance.rs
@@ -0,0 +1,40 @@
+use super::*;
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Alliance {
+    pub creator_corporation_id: i32,
+    pub creator_id: i32,
+    pub date_founded: String,
+    pub executor_corporation_id: Option<i32>,
+    pub faction_id: Option<i32>,
+    pub name: String,
+    pub ticker: String,
+}
+impl Alliance {
+    pub async fn from(id: i32) -> anyhow::Result<Self> {
+        let url = format!("{EVE_TECH_ROOT}/alliances/{id}/?{EVE_TECH_SERVER}");
+        info!("{url}");
+        reqwest::get(&url)
+            .await?
+            .json::<Self>()
+            .await
+            .map_err(|e| anyhow!(e))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn from() -> Result<(), String> {
+        let corporation = Alliance::from(99003581).await.map_err(|e| format!("{e}"))?;
+
+        assert_eq!(corporation.creator_corporation_id, 98241771);
+        assert_eq!(corporation.creator_id, 379226154);
+        assert_eq!(&corporation.date_founded, "2013-08-23T05:45:11Z");
+        assert_eq!(&corporation.name, "Fraternity.");
+        assert_eq!(&corporation.ticker, "FRT");
+        Ok(())
+    }
+}
--- src/zkbgui.rs
+++ src/zkbgui.rs
@@ -1,19 +1,24 @@
 #[macro_use]
-extern crate actix_web;
+extern crate handlebars;
 
+use actix_cors::Cors;
 use actix_files::Files;
+use actix_web::{get, http};
 use actix_web::{web, App, HttpResponse, HttpServer};
 use anyhow::anyhow;
+use chrono::NaiveDateTime;
 use handlebars::Handlebars;
 use log::{error, info};
 use serde::{Deserialize, Serialize};
-use chrono::NaiveDateTime;
 
 use lib::evetech::Character;
-use lib::evetech::Corporation;
 use lib::evetech::CharacterPortrait;
 use lib::evetech::SearchCategory;
 use lib::evetech::SearchResult;
+
+// use lib::api::Wins;
+// use lib::api::Losses;
+// use lib::api::Activity;
 
 use std::env;
 
@@ -28,13 +33,24 @@ async fn main() -> anyhow::Result<()> {
         .parse::<u16>()
         .unwrap_or(8088);
 
+    handlebars::handlebars_helper!(not_zero: |id: i32| id != 0);
+
     let mut handlebars = Handlebars::new();
     handlebars.register_templates_directory(".html", "./public/templates")?;
+    handlebars.register_helper("not_zero", Box::new(not_zero));
     let context = web::Data::new(handlebars);
 
     info!("Try http://{host}:{port}/");
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://zkbinfo/")
+            .allowed_origin_fn(|origin, _req_head| origin.as_bytes().ends_with(b".rust-lang.org"))
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
         App::new()
+            .wrap(cors)
             .app_data(context.clone())
             .service(Files::new("/css", "./public/css").show_files_listing())
             .service(Files::new("/js", "./public/js").show_files_listing())
@@ -89,13 +105,7 @@ struct CharacterProps {
     character_security_status: String,
 
     corporation_id: i32,
-    corporation_name: String,
-    corporation_ticker: String,
-    corporation_members: i32,
-
-
-    // pub alliance_id: Option<i32>,
-    // pub faction_id: Option<i32>,
+    alliance_id: i32,
 
     img_64x64: String,
     img_128x128: String,
@@ -108,12 +118,11 @@ impl CharacterProps {
             .get_character_id()?;
 
         let character = Character::from(id).await?;
-        let corporation = Corporation::from(character.corporation_id).await?;
         let portrait = CharacterPortrait::from(id).await?;
         let parse_date = NaiveDateTime::parse_from_str;
         let birthday = parse_date(&character.birthday, "%Y-%m-%dT%H:%M:%SZ")?;
 
-        Ok(Self {
+        let props = Self {
             character_id: id,
             character_name: character.name,
             character_gender: character.gender,
@@ -121,14 +130,15 @@ impl CharacterProps {
             character_security_status: format!("{:.2}", character.security_status),
 
             corporation_id: character.corporation_id,
-            corporation_name: corporation.name,
-            corporation_ticker: corporation.ticker,
-            corporation_members: corporation.member_count,
+
+            alliance_id: character.alliance_id.unwrap_or_default(),
 
             img_64x64: portrait.px64x64,
             img_128x128: portrait.px128x128,
             img_256x256: portrait.px256x256,
-        })
+        };
+
+        Ok(props)
     }
 }
 
