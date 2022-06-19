69ff7de990f9ef307d4556d6d96b9587b6a70074
Seb <seb@ukr.net>
2022-06-19 16:12:29 +0300

    Intermediate state of the Character Report

--- public/js/script.js
+++ public/js/script.js
@@ -28,56 +28,26 @@ function most_active(obj, count) {
     return map;
 }
 
-function format(activities) {
-
+function format(prefix, systems, ships, damage) {
     let html = [];
-    html.push("<p>Wins (" + activities.wins.killmails.length + "): ");
-    for (let i = 0; i < activities.wins.killmails.length; i++) {
-        const id = activities.wins.killmails[i];
-        html.push(`<a href="https://zkillboard.com/kill/${id}/">${id}</a> `);
-    }
-    html.push("</p>");
-    html.push("<p>Total damage dealt: " + activities.wins.total_damage + "</p>");
+    // html.push("<p>(" + report.killmails.length + "): ");
+    // for (let i = 0; i < report.killmails.length; i++) {
+    //     const id = report.killmails[i];
+    //     html.push(`<a href="https://zkillboard.com/kill/${id}/">${id}</a> `);
+    // }
+    // html.push("</p>");
+    html.push("<p>Total damage: " + damage + "</p>");
     html.push("<div>Systems with most activities: ")
-    const wins_solar_systems = most_active(activities.wins.solar_systems, 5);
-    wins_solar_systems.forEach((count, system_id) => {
-        html.push(`<div id="wins_${system_id}" div style="display: inline">*</div> `);
+    systems.forEach((count, system_id) => {
+        html.push(`<div id="${prefix}_${system_id}" div style="display: inline">*</div> `);
     });
     html.push("</div>");
+
     html.push("<div>Favorite ships: ");
-    const wins_ships = most_active(activities.wins.ships, 5);
-    wins_ships.forEach((count, ship_id) => {
-        html.push(`<div id="wins_${ship_id}" div style="display: inline">*</div> `);
+    ships.forEach((count, ship_id) => {
+        html.push(`<div id="${prefix}_${ship_id}" div style="display: inline">*</div> `);
     });
     html.push("</div>");
 
-
-    requestNamesAsync(Array.from(wins_solar_systems.keys()))
-        .then(names => {
-            names.forEach((obj) => {
-                const id = obj.id;
-                const name = obj.name;
-                const count = wins_solar_systems.get(`${id}`);
-                const href = `<a href="https://zkillboard.com/system/${id}/">${name} (${count})</a>`;
-                const element = `wins_${id}`;
-                document.getElementById(element).innerHTML = href;
-            });
-        });
-
-    requestNamesAsync(Array.from(wins_ships.keys()))
-        .then(names => {
-            names.forEach((obj) => {
-                const id = obj.id;
-                const name = obj.name;
-                const count = wins_ships.get(`${id}`);
-                const href = `<a href="https://zkillboard.com/ship/${id}/">${name} (${count})</a>`;
-                const element = `wins_${id}`;
-                document.getElementById(element).innerHTML = href;
-            });
-        });
     return html.join("");
-}
-
-function format_wins(activities) {
-
 }
