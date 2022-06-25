8e5f4d3d47582a9e100cf85abcc52f189aadfaa6
Seb <seb@ukr.net>
2022-06-25 06:40:47 +0300

    Backward gather script added

---
+++ scripts/update_to_past.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+
+
+CURRENT=${1:-$(date +'%Y-%m-%d' -d "1 day ago")}
+FINISH=${2:-$(date +'%Y-%m-%d' -d "1 year ago")}
+if [ "$CURRENT" != "$TODAY" ]; then
+    while [ "$CURRENT" != "$FINISH" ]; do
+      echo $CURRENT
+      ZKBINFO_HOST=zkbinfo ~/zkbinfo/target/release/fetch_by_date $CURRENT
+      CURRENT=$(date -I -d "$CURRENT - 1 day")
+    done
+fi
